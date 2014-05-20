056b20f82294f7c4540992fb691533832ad95874
FrankSt <noreply>
2014-05-20 12:32:25 +0000

    Add component taak

---
+++ ui/taak.reel/taak.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/taak.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Taak
+ * @extends Component
+ */
+exports.Taak = Component.specialize(/** @lends Taak# */ {
+    constructor: {
+        value: function Taak() {
+            this.super();
+        }
+    }
+});
